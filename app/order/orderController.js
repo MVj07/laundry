2312aea7b7ae1a6a6912cbe00ab88a3094be67a5
maduraiveeran <maduraiveeran@vprc.in>
2025-08-07 21:09:50 +0530

    new_changes

--- app/order/orderController.js
+++ app/order/orderController.js
@@ -109,7 +109,7 @@ const updateOrder = async (req, res, next) => {
 
 const getAll = async(req, res, next) => {
     try {
-        const item = await orders.find()
+        const item = await orders.find().populate('customerId')
         return res.status(200).json({
             data: item
         })
