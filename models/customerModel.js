6670cca9ce02419152a1f55df140d3c26a289517
maduraiveeran <maduraiveeran@vprc.in>
2025-08-26 21:31:33 +0530

    new

--- models/customerModel.js
+++ models/customerModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const customerSchema = new mongoose.Schema({
     name: { type: String, required: true },
     address: { type: String },
-    mobile: { type: String, required: true },
+    mobile: { type: String, required: true, unique: true },
     // kuri: { type: Number, required: true },
     // createdAt: { type: Date },
     // updatedAt: { type: Date }
